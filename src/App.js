f3d852d729874ccef2f1b0dae78c2c0445149924
Sagar Samanta <sagarsamanta390@gmail.com>
2023-05-28 08:59:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import {
   withScriptjs,
   GoogleMap,
   Marker,
-  InfoWindow
+  InfoWindow,
 } from "react-google-maps";
-import * as parkData from "./data/skateboard-parks.json";
-import mapStyles from "./mapStyles";
+import * as parkData from "./data/fakeData.json";
+// import mapStyles from "./mapStyles";
 
 function Map() {
   const [selectedPark, setSelectedPark] = useState(null);
 
   useEffect(() => {
-    const listener = e => {
+    const listener = (e) => {
       if (e.key === "Escape") {
         setSelectedPark(null);
       }
@@ -27,24 +27,24 @@ function Map() {
 
   return (
     <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
-      defaultOptions={{ styles: mapStyles }}
+      defaultZoom={5}
+      defaultCenter={{ lat: 20.5937, lng: 78.9629 }}
+      // defaultOptions={{ styles: mapStyles }}
     >
-      {parkData.features.map(park => (
+      {parkData.features.map((park) => (
         <Marker
-          key={park.properties.PARK_ID}
+          key={park.properties.ID}
           position={{
-            lat: park.geometry.coordinates[1],
-            lng: park.geometry.coordinates[0]
+            lat: park.geometry.coordinates[0],
+            lng: park.geometry.coordinates[1],
           }}
           onClick={() => {
             setSelectedPark(park);
           }}
-          icon={{
-            url: `/skateboarding.svg`,
-            scaledSize: new window.google.maps.Size(25, 25)
-          }}
+          // icon={{
+          //   url: `/skateboarding.svg`,
+          //   scaledSize: new window.google.maps.Size(25, 25),
+          // }}
         />
       ))}
 
@@ -54,13 +54,13 @@ function Map() {
             setSelectedPark(null);
           }}
           position={{
-            lat: selectedPark.geometry.coordinates[1],
-            lng: selectedPark.geometry.coordinates[0]
+            lat: selectedPark.geometry.coordinates[0],
+            lng: selectedPark.geometry.coordinates[1],
           }}
         >
           <div>
             <h2>{selectedPark.properties.NAME}</h2>
-            <p>{selectedPark.properties.DESCRIPTIO}</p>
+            {/* <p>{selectedPark.properties.DESCRIPTIO}</p> */}
           </div>
         </InfoWindow>
       )}
@@ -72,11 +72,11 @@ const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div style={{ width: "50%", height: "100vh" }}>
       <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.REACT_APP_GOOGLE_KEY
-        }`}
+        googleMapURL={
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyCJnQzocSDXHYNSPG71tejT2PRo047ehro&v=3.exp&libraries=geometry,drawing,places"
+        }
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
